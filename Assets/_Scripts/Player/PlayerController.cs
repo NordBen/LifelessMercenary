c5462abd150560979d0b694b8258485ef0f36b82
mbl <mikaellaegran@gmail.com>
2025-04-04 10:52:42 +0200

    Deleted prefab

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Xml.Serialization;
 using Unity.Mathematics;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,7 +11,14 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float sprintTransitSpeed = 5f; // How fast the caracter transitions to sprint
     [SerializeField] private float turningSpeed = 10f;
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float jumpHeight = 2f;
+
+    private float verticalVelocity; 
+    private float speed;
 
     [Header("Input")]
     private float moveInput;
@@ -38,9 +46,19 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(moveInput, 0, turnInput);
         move = transform.TransformDirection(move);
 
-        move.y = 0;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = Mathf.Lerp(speed, sprintSpeed, sprintTransitSpeed * Time.deltaTime);
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, walkSpeed, sprintTransitSpeed * Time.deltaTime);
 
-        move *= walkSpeed;
+        }
+
+        move *= speed;
+
+        move.y = VerticalForceCalculation();
 
         characterController.Move(move * Time.deltaTime);
     }
@@ -58,6 +76,24 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turningSpeed);   
 
         }
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (characterController.isGrounded)
+        {
+            verticalVelocity = -1;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        return verticalVelocity;
     }
 
     private void InputManagement()
